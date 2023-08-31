5cf3577fcd78ca4a1dad105eecf9b9244040cf43
Angel <angel.yordanov.pavlov@gmail.com>
2023-08-31 16:00:50 +0300

    Update

--- src/Web/User.java
+++ src/Web/User.java
@@ -1,18 +1,13 @@
 package Web;
 
-
-import StartingWindow.TextFieldURL;
 import org.apache.commons.codec.binary.Base64;
 import org.jsoup.Connection;
-import org.jsoup.HttpStatusException;
 import org.jsoup.Jsoup;
-
 import java.io.IOException;
 
 public class User {
 
     private static final String LOGIN = "shipping:shipping";
-
 
     User() {
 
@@ -29,22 +24,19 @@ public class User {
                             "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:41.0) Gecko/20100101 Firefox/41.0")
                     .header("Authorization", "Basic " + base64login)
                     .execute();
-           WebSite.setDocument(response.parse());
+            WebSite.setDocument(response.parse());
             System.out.println(WebSite.getDocument().text());
             WebSite.setIsRealUrl(true);
 
-
-        } catch (Exception e) {
-           WebSite.setIsRealUrl(false);
-            e.printStackTrace();
-
+        } catch (org.jsoup.HttpStatusException | java.lang.IllegalArgumentException ex) {
+            WebSite.setIsRealUrl(false);
+            // ex.printStackTrace();
 
         }
-        if (WebSite.getIsRealUrl() == false){
-            System.out.println("GRESHKA");
-        }
-        else {
-            System.out.println("VQRNO");
+        if (WebSite.getIsRealUrl() == false) {
+
+        } else {
+
         }
     }
 }
